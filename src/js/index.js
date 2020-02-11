840cecd79034421357d4fe96e3983fc946d0656c
Mauricio Cavalleri <maurimanuel92@gmail.com>
2020-02-11 19:50:36 +0000

    Añadida musica al pulsar la carátula del vinilo

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 var x = document.getElementById("algorithm_8bits_audio"); 
 
-function playAudio() { 
-  x.play(); 
-} 
 
-function pauseAudio() { 
-  x.pause(); 
-} 
+document.getElementById("cover").addEventListener("mousedown", function(){ 
+    x.play(); 
+});
+
+document.getElementById("cover").addEventListener("mouseup", function(){ 
+    x.pause(); 
+});
